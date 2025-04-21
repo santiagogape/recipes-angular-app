3cc5510255f6f94e9dc3c1471cda8f7978f93753
santiagogape <santiago.galindo101@alu.ulpgc.es>
2025-04-21 19:49:26 +0100

    no footer en profile

--- src/app/pages/my/profile/profile.component.ts
+++ src/app/pages/my/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {ProfileContentComponent} from '../../../components/profile-content/profile-content.component';
-import {FooterComponent} from '../../../components/general/footer/footer.component';
 
 @Component({
   selector: 'app-profile',
